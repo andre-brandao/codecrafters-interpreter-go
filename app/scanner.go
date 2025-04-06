499b35fdcb59393f727fa29569b8833c7c440539
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-04-06 17:12:50 -0300

    whitespace + or + strings

--- app/scanner.go
+++ app/scanner.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 type Scanner struct {
@@ -58,10 +59,71 @@ func (s *Scanner) match(expected rune) bool {
 }
 
 func (s *Scanner) peek() rune {
-    if s.isAtEnd() {
-        return '\000'
-    }
-    return s.source[s.current]
+	if s.isAtEnd() {
+		return '\000'
+	}
+	return s.source[s.current]
+}
+
+func (s *Scanner) peekNext() rune {
+	if s.current+1 >= len(s.source) {
+		return '\000'
+	}
+	return s.source[s.current+1]
+}
+
+func (s *Scanner) string() {
+	for s.peek() != '"' && !s.isAtEnd() {
+		if s.peek() == '\n' {
+			s.line++
+		}
+		s.advance()
+	}
+
+	if s.isAtEnd() {
+		error(s.line, "Unterminated string.")
+		return
+	}
+
+	s.advance()
+	value := s.source[s.start+1 : s.current-1]
+	s.addToken(STRING, value)
+}
+
+func (s *Scanner) number() {
+	for isDigit(s.peek()) {
+		s.advance()
+	}
+	if s.peek() == '.' && isDigit(s.peekNext()) {
+		// Consume the "."
+		s.advance()
+
+		for isDigit(s.peek()) {
+			s.advance()
+		}
+	}
+
+	numStr := string(s.source[s.start:s.current])
+	val, err := strconv.ParseFloat(numStr, 64)
+	if err != nil {
+		error(s.line, "Invalid number format.")
+		return
+	}
+	s.addToken(NUMBER, val)
+}
+
+func (s *Scanner) identifier() {
+	for isAlphaNumeric(s.peek()) {
+		s.advance()
+	}
+
+	text := string(s.source[s.start:s.current])
+	type_, ok := keywords[text]
+
+	if !ok {
+		type_ = IDENTIFIER
+	}
+	s.addToken(type_, nil)
 }
 
 func (s *Scanner) scanToken() {
@@ -122,9 +184,25 @@ func (s *Scanner) scanToken() {
 		} else {
 			s.addToken(SLASH, nil)
 		}
+	case ' ', '\r', '\t':
+		// Ignore whitespace.
+		break
+	case '\n':
+		s.line++
+	case '"':
+		s.string()
+
+	case 'o':
+		if s.match('r') {
+			s.addToken(OR, nil)
+		}
 
 	default:
-		error(s.line, fmt.Sprintf("Unexpected character: %c", c))
+		if isDigit(c) {
+			s.number()
+		} else {
+			error(s.line, fmt.Sprintf("Unexpected character: %c", c))
+		}
 	}
 }
 
@@ -137,3 +215,4 @@ func (s *Scanner) addToken(t TokenType, literal any) {
 		Line:    s.line,
 	})
 }
+
--- app/token.go
+++ app/token.go
@@ -56,6 +56,25 @@ const (
 	EOF
 )
 
+var keywords = map[string]TokenType{
+	"and":    AND,
+	"class":  CLASS,
+	"else":   ELSE,
+	"false":  FALSE,
+	"for":    FOR,
+	"fun":    FUN,
+	"if":     IF,
+	"nil":    NIL,
+	"or":     OR,
+	"print":  PRINT,
+	"return": RETURN,
+	"super":  SUPER,
+	"this":   THIS,
+	"true":   TRUE,
+	"var":    VAR,
+	"while":  WHILE,
+}
+
 func (t TokenType) String() string {
 	switch t {
 	case LEFT_PAREN:
