8ae40b07c99b635e2a86ea632ca41848b9812790
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-04-06 14:08:25 -0300

    add scanner and token

---
+++ app/scanner.go
@@ -0,0 +1,127 @@
+package main
+
+type Scanner struct {
+	source  []rune
+	tokens  []Token
+	start   int
+	current int
+	line    int
+}
+
+func NewScanner(source []rune) *Scanner {
+	return &Scanner{
+		source:  source,
+		tokens:  make([]Token, 0),
+		start:   0,
+		current: 0,
+		line:    1,
+	}
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.current >= len(s.source)
+}
+
+func (s *Scanner) ScanTokens() []Token {
+	for !s.isAtEnd() {
+		// We are at the beginning of the next lexeme.
+		s.start = s.current
+		s.scanToken()
+	}
+
+	s.tokens = append(s.tokens, Token{
+		Type:    EOF,
+		Lexeme:  []rune(""),
+		Literal: nil,
+		Line:    s.line,
+	})
+
+	return s.tokens
+}
+
+func (s *Scanner) advance() rune {
+	r := s.source[s.current]
+	s.current++
+	return r
+}
+
+func (s *Scanner) match(expected rune) bool {
+	if s.isAtEnd() || s.source[s.current] != expected {
+		return false
+	}
+	s.current++
+	return true
+}
+
+func (s *Scanner) scanToken() {
+	c := s.advance()
+	switch c {
+	case '(':
+		s.addToken(LEFT_PAREN, nil)
+	case ')':
+		s.addToken(RIGHT_PAREN, nil)
+	case '{':
+		s.addToken(LEFT_BRACE, nil)
+	case '}':
+		s.addToken(RIGHT_BRACE, nil)
+	case ',':
+		s.addToken(COMMA, nil)
+	case '.':
+		s.addToken(DOT, nil)
+	case '-':
+		s.addToken(MINUS, nil)
+	case '+':
+		s.addToken(PLUS, nil)
+	case ';':
+		s.addToken(SEMICOLON, nil)
+	case '*':
+		s.addToken(STAR, nil)
+	case '!':
+		if s.match('=') {
+			s.addToken(BANG_EQUAL, nil)
+		} else {
+			s.addToken(BANG, nil)
+		}
+	case '=':
+		if s.match('=') {
+			s.addToken(EQUAL_EQUAL, nil)
+		} else {
+			s.addToken(EQUAL, nil)
+		}
+	case '<':
+		if s.match('=') {
+			s.addToken(LESS_EQUAL, nil)
+		} else {
+			s.addToken(LESS, nil)
+		}
+	case '>':
+		if s.match('=') {
+			s.addToken(GREATER_EQUAL, nil)
+		} else {
+			s.addToken(GREATER, nil)
+		}
+	case '/':
+		if s.match('/') {
+			// A comment goes until the end of the line.
+			// while (peek() != '\n' && !isAtEnd()) advance();
+			//
+
+		} else {
+			s.addToken(SLASH, nil)
+		}
+		break
+
+	default:
+		error(s.line, "Unexpected character.")
+	}
+}
+
+func (s *Scanner) addToken(t TokenType, literal any) {
+	text := s.source[s.start:s.current]
+	s.tokens = append(s.tokens, Token{
+		Type:    t,
+		Lexeme:  text,
+		Literal: literal,
+		Line:    s.line,
+	})
+}
